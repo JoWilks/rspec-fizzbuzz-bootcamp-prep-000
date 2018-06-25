4d1bfe842cfa9d0380fe089722eaaaa5a6bc6a77
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-25 12:26:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 def fizzbuzz(int)
   if (int % 3 == 0 && int % 5 ==0)
   "FizzBuzz"
- end
+  end
 else if int % 3 == 0 
- return  "Fizz"
- end
+  "Fizz"
+  end
 
 else if int % 5 == 0 
- return  "Buzz"
+   "Buzz"
   end 
 end
